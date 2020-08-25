585f66370c86a09e09065521f9d0612f499b4d1b
Charles Forcat <charles.forcat@gmail.com>
2020-08-25 09:06:17 +0200

    Edited README

---
+++ Angular_WorkspaceBooking/src/app/booking.ts
@@ -0,0 +1,12 @@
+export class Booking {
+
+    Available : string;
+    Estimate : string;
+}
+
+export class OfferItem {
+    constructor(
+      public available : string,
+      public bestoffer: string
+    ) {}
+  }
